1f1312c0110b4c8a9b1aab877523e2e15ee76210
BFandLee <vactor224@gmail.com>
2024-10-11 19:01:54 +0900

    네이버 지도 API 추가 + 핸드폰에서 네이버 지도 뜨는지 체크 완료

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.retrofit)        // Retrofit 의존성 추가
     implementation(libs.retrofit.gson)   // Gson 컨버터 의존성 추가
+    implementation(libs.naver.map.sdk)      // 네이버 지도 SDK 의존성 추가
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
