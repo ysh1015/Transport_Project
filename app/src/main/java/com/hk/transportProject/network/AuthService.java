3591a0b6f1457fa85d49243f783b83f4c224991e
TaeGyun_Kim <134123228+monm12@users.noreply.github.com>
2024-10-15 22:07:58 +0900

    Unit Test

--- app/src/main/java/com/hk/transportProject/network/AuthService.java
+++ app/src/main/java/com/hk/transportProject/network/AuthService.java
@@ -9,7 +9,7 @@ import retrofit2.http.POST;
 
 
 public interface AuthService {
-
+    // 추가 예정
     @POST("로그인 할 곳")
     Call<LoginResponse> login(@Body User user);
 }
--- app/src/test/java/com/hk/transportProject/AuthRepositoryTest.java
+++ app/src/test/java/com/hk/transportProject/AuthRepositoryTest.java
@@ -47,17 +47,33 @@ public class AuthRepositoryTest {
         LoginResponse mockResponse = new LoginResponse(true, "로그인 성공");
 
         Call<LoginResponse> mockCall = mock(Call.class);
-        when(authService.login(any(User.class))).thenReturn(mockCall);
+        // when(authService.login(any(User.class))).thenReturn(mockCall);
+
+        when(authService.login(any(User.class))).thenAnswer(invocation -> {
+            // 확인용 println
+            User userArgument = invocation.getArgument(0);
+            System.out.println("AuthRepositoryTestSuccess - ID: " + userArgument.getUserId());
+            System.out.println("AuthRepositoryTestSuccess - Password: " + userArgument.getPassword());
+
+            doAnswer(invocation1 -> {
+                Callback<LoginResponse> callback = invocation1.getArgument(0);
+                callback.onResponse(mockCall, Response.success(mockResponse));
+                return null;
+            }).when(mockCall).enqueue(any(Callback.class));
+
+            return mockCall;
+            });
 
         // Retrofit Call 객체 enqueue 직접 제어
+        /*
         doAnswer(invocation -> {
             Callback<LoginResponse> callback = invocation.getArgument(0);
             callback.onResponse(mockCall, Response.success(mockResponse));
             return null;
         }).when(mockCall).enqueue(any(Callback.class));
+        */
 
         // when
-
         LiveData<LoginResponse> responseLiveData = authRepository.login(user);
 
         // Then
@@ -72,18 +88,34 @@ public class AuthRepositoryTest {
         LoginResponse mockResponse = new LoginResponse(false, "로그인 실패");
 
         Call<LoginResponse> mockCall = mock(Call.class);
-        when(authService.login(any(User.class))).thenReturn(mockCall);
+        when(authService.login(any(User.class))).thenAnswer(invocation -> {
+            // 확인용 println
+            User userArgument = invocation.getArgument(0);
+            System.out.println("AuthRepositoryTestSuccess - ID: " + userArgument.getUserId());
+            System.out.println("AuthRepositoryTestSuccess - Password: " + userArgument.getPassword());
 
+            doAnswer(invocation1 -> {
+                Callback<LoginResponse> callback = invocation1.getArgument(0);
+                callback.onResponse(mockCall, Response.success(mockResponse));
+                return null;
+            }).when(mockCall).enqueue(any(Callback.class));
+
+            return mockCall;
+        });
+
+        /*
         doAnswer(invocation -> {
             Callback<LoginResponse> callback = invocation.getArgument(0);
             callback.onResponse(mockCall, Response.success(mockResponse));
             return null;
         }).when(mockCall).enqueue(any(Callback.class));
-
+        */
         // When
         LiveData<LoginResponse> responseLiveData = authRepository.login(user);
 
         // Then
+        assertNotNull(responseLiveData);
+        assertNotNull(responseLiveData.getValue());
         assertEquals(false, responseLiveData.getValue().isSuccess());
         assertEquals("로그인 실패", responseLiveData.getValue().getMessage());
     }
